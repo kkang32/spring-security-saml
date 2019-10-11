0a90bbec538f40182b836f49c575dafa7a475779
kkang32 <kkang32@gmail.com>
2019-10-11 16:51:37 +0900

    spring security saml sample

---
+++ src/main/java/org/springframework/security/saml/web/HelloController.java
@@ -0,0 +1,14 @@
+package org.springframework.security.saml.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
